3750c209bc232cba71a85542a0d16f3179a6d272
velovan <velovan@users.noreply.github.com>
2017-08-14 19:41:14 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
     private static Logger log = LogManager.getLogger(Main.class);
 
     public static void main(String[] ar) {
+     
         LocalTime lt = LocalTime.now();
         String output = Greeting.getGreeting(lt.getHour());
         log.trace("Getting an output: " + output);
