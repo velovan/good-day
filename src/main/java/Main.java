567471504fb10f72cb214f85100ad33872e4bdc2
velovan <velovan@users.noreply.github.com>
2022-03-21 13:09:16 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,14 @@ import org.apache.logging.log4j.Logger;
 
 import java.time.LocalTime;
  
-public class Main {
+public class Main{
+ 
     private static Logger log = LogManager.getLogger(Main.class);
-
-    public static void main(String[] ar) {
-     
+    public static void main(String[] ar){
         LocalTime lt = LocalTime.now();
         String output = Greeting.getGreeting(lt.getHour());
         log.trace("Getting an output: " + output);
         System.out.print(output);
     }
+ 
 }
